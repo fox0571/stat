82e44989f5d1cea9529868c050a1e6f74315b8f5
fox0571 <fox0571@users.noreply.github.com>
2018-08-03 18:12:59 -0500

    front-stat

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Hello></Hello>
+    <Hello></Hello>
+    <Hello></Hello>
+    <Hello></Hello>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Hello from './components/HelloWorld'
+Vue.use(Element)
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data: function () {
+    return {
+      active: true
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style>
+  body{margin: 0;}
+  #app {
+    min-width: 1200px;
+    margin: 0 auto;
+    font-family: "Helvetica Neue","PingFang SC",Arial,sans-serif;
+  }
+
+</style>
